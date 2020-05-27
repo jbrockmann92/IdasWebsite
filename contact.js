e9702e493d5b773938e8f3b7978d2ec195088425
Jacob Brockmann <59708272+jbrockmann92@users.noreply.github.com>
2020-05-27 13:01:12 -0500

    Delete contact.js

--- contact.js
+++
@@ -1,8 +0,0 @@
-function sendEmail(){
-    var fName = document.getElementById('fName').value;
-    var lName = document.getElementById('lName').value;
-    var email = document.getElementById('email').value;
-    var comment = document.getElementById('subject').value;
-
-    console.log(email, fName, lName, comment);
-}

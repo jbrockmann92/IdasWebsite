f0d59ef5885ecdf1677d99654852d4ff9e9cf1a8
Jacob <jacob.brockmann92@gmail.com>
2020-05-27 12:58:59 -0500

    Added gitignore file and wrote a bit of js

--- contact.js
+++ contact.js
@@ -1,33 +1,8 @@
 function sendEmail(){
-    var nodemailer = require('nodemailer');
+    var fName = document.getElementById('fName').value;
+    var lName = document.getElementById('lName').value;
+    var email = document.getElementById('email').value;
+    var comment = document.getElementById('subject').value;
 
-    requirejs.config({
-        //Pass the top-level main.js/index.js require
-        //function to requirejs so that node modules
-        //are loaded relative to the top-level JS file.
-        nodeRequire: require
-    });
-
-    var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'idasengineering@gmail.com',
-        pass: ''
-    }
-    });
-
-    var mailOptions = {
-    from: 'idasengineering@gmail.com',
-    to: 'idasengineering@gmail.com',
-    subject: 'Testing for NodeMailer',
-    text: document.getElementById('subject'),
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-    });
+    console.log(email, fName, lName, comment);
 }
